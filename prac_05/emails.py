69e52598096c15dd868c3341b59d4a44a27048aa
lzhpeng <160458660+lzhpeng@users.noreply.github.com>
2025-06-19 18:59:01 +0800

    This step is storing the email and name in a dictionary.

---
+++ prac_05/emails.py
@@ -0,0 +1,12 @@
+def main():
+    email_to_name = {}
+    email = input("Enter your email address: ")
+    while email != "":
+        name = get_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ")
+        if confirmation.upper() != "Y" and confirmation != "":
+            name = input("Enter your name: ")
+        email_to_name[email] = name
+        email = input("Enter your email address: ")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
