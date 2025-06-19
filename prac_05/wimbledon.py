9bad3c3a5b2dec1e95f67bdb4f1b3a15a58f6160
lzhpeng <160458660+lzhpeng@users.noreply.github.com>
2025-06-19 19:20:13 +0800

    Read each line from the file (excluding the header) and store it as a list in records.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -28,3 +28,16 @@ def display_results(champion_to_count, countries):
         print(name, count)
     print(f"\nThese {len(countries)} countries have won Wimbledon: ")
     print(", ".join(sorted(countries)))
+
+def load_records(filename):
+    """Load records from file in list of lists form."""
+    records = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # Remove CSV header row
+        for line in in_file:
+            parts = line.strip().split(",")
+            records.append(parts)
+    return records
+
+
+main()
