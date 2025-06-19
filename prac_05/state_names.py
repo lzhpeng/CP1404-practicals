7ccd1d4a9b47c02bc572d9c44c5bfd099d2da7e3
lzhpeng <160458660+lzhpeng@users.noreply.github.com>
2025-06-19 19:09:30 +0800

    repeatedly prompts the user to enter a state abbreviation and prints the full state name

---
+++ prac_05/state_names.py
@@ -0,0 +1,4 @@
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales",
+                "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria",
+                "TAS": "Tasmania", "SA": "South Australia"}
