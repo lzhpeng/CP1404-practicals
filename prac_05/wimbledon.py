ee3147b2e1c18c27c9a24726a422e109e94429a0
lzhpeng <160458660+lzhpeng@users.noreply.github.com>
2025-06-19 19:19:06 +0800

    Count the number of wins for each champion and collect all champion countries.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -8,3 +8,15 @@ def main():
     records = load_records(FILENAME)
     champion_to_count, countries = process_records(records)
     display_results(champion_to_count, countries)
+
+def process_records(records):
+    """Create dictionary of champions and set of countries from records (list of lists)."""
+    champion_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[INDEX_COUNTRY])
+        try:
+            champion_to_count[record[INDEX_CHAMPION]] += 1
+        except KeyError:
+            champion_to_count[record[INDEX_CHAMPION]] = 1
+    return champion_to_count, countries
