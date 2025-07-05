cb650976888c07a3fb4873a02d8aef83ac4def9a
lzhpeng <160458660+lzhpeng@users.noreply.github.com>
2025-07-05 13:36:31 +0800

    Set up a new car with name and fuel.

--- prac_06/car.py
+++ prac_06/car.py
@@ -6,27 +6,3 @@ class Car:
         self.name = name
         self.fuel = fuel
         self._odometer = 0
-
-    def __str__(self):
-        """
-        Return car details as a string.
-        """
-        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
-
-    def add_fuel(self, amount):
-        """
-        Add fuel to the car.
-        """
-        self.fuel += amount
-
-    def drive(self, distance):
-        """
-        Drive the car and update fuel and odometer.
-        """
-        if distance > self.fuel:
-            distance = self.fuel
-            self.fuel = 0
-        else:
-            self.fuel -= distance
-        self._odometer += distance
-        return distance
