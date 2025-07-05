9e4fa2a59325a61c09b43e78c90bcbbf27676cf3
lzhpeng <160458660+lzhpeng@users.noreply.github.com>
2025-07-05 14:04:31 +0800

    already on the feedback

---
+++ prac_06/guitars.py
@@ -0,0 +1,28 @@
+from prac_06.guitar import Guitar
+
+
+def main():
+    guitars = []
+    print("My guitars!")
+    name = input("Name (leave blank to finish): ").strip()
+    while name:
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} added.")
+        name = input("Name (leave blank to finish): ").strip()
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    if guitars:
+        print("\nThese are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = " (vintage)" if guitar.is_vintage() else ""
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+    else:
+        print("No guitars :( Quick, go and buy one!")
+
+
+if __name__ == "__main__":
+    main()
