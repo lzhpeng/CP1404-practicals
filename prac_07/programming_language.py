f2ba6032192b6be0e4c7817bdc894a73addbdb68
lzhpeng <160458660+lzhpeng@users.noreply.github.com>
2025-07-13 11:48:58 +0800

    Run simple tests/demos on ProgrammingLanguage class.

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -15,3 +15,22 @@ class ProgrammingLanguage:
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
+
+
+def run_tests():
+    """Run simple tests/demos on ProgrammingLanguage class."""
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [ruby, python, visual_basic]
+    print(python)
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+if __name__ == "__main__":
+    run_tests()
