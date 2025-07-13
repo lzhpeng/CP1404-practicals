840824d25dbaf6bf64e5832ed9b6c26efdf64847
lzhpeng <160458660+lzhpeng@users.noreply.github.com>
2025-07-13 11:47:58 +0800

    Represent information about a programming language and  construct a ProgrammingLanguage from the given values

---
+++ prac_07/programming_language.py
@@ -0,0 +1,9 @@
+class ProgrammingLanguage:
+    """Represent information about a programming language."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Construct a ProgrammingLanguage from the given values."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
