3f5ac8a12f20b2387df49f02a2ab48cf5b20a525
lzhpeng <160458660+lzhpeng@users.noreply.github.com>
2025-07-27 13:44:36 +0800

    Run tests for Car, Taxi and SilverServiceTaxi classes.

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -52,3 +52,38 @@ def display_taxis(taxis):
     """Display all available taxis with their indices."""
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
+
+
+def run_tests():
+    """Run tests for Car, Taxi and SilverServiceTaxi classes."""
+    bus = Car("Datsun", 180)
+    bus.drive(30)
+    print("fuel =", bus.fuel)
+    print("odo =", bus._odometer)
+    bus.drive(55)
+    print("fuel =", bus.fuel)
+    print("odo = ", bus._odometer)
+    print(bus)
+
+    distance = int(input("Drive how far? "))
+    while distance > 0:
+        distance_travelled = bus.drive(distance)
+        print(f"{bus} travelled {distance_travelled}")
+        distance = int(input("Drive how far? "))
+
+    t = Taxi("Prius 1", 100)
+    print(t)
+    t.drive(25)
+    print(t, t.get_fare())
+    t.start_fare()
+    t.drive(40)
+    print(t, t.get_fare())
+
+    sst = SilverServiceTaxi("Limo", 100, 2)
+    print(sst, sst.get_fare())
+    sst.drive(10)
+    print(sst, sst.get_fare())
+
+
+if __name__ == "__main__":
+    main()
